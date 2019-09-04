32da01c467e2415e97ecbe8c93f71ec73d4b1312
VISHNU <VISHNU@VATSA>
2019-09-04 23:22:47 +0530

    Added a class and sum

---
+++ TestProject/src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		float b=9.5f;
+		double sum;
+		sum=a+b;
+		System.out.println(sum);
+	}
+
+}
